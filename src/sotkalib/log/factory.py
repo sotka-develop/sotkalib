a4bc785d8490e3926315266c610531e94954e400
pyro <me@pyrorhythm.dev>
2026-02-04 17:31:29 +0300

    release: v0.0.5.post3

--- src/sotkalib/log/factory.py
+++ src/sotkalib/log/factory.py
@@ -1,8 +1,8 @@
 from functools import lru_cache
 
-from loguru import Logger, logger
+from loguru import logger
 
 
 @lru_cache
-def get_logger(logger_name: str | None = None) -> Logger:
+def get_logger(logger_name: str | None = None):
 	return logger if logger_name is None else logger.bind(name=logger_name.replace(".", " -> "))
